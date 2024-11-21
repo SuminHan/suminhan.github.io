a1b2586431df2598cc66aad73d9c5eb08be0c5fe
Sumin Han <hsm6911@kaist.ac.kr>
2024-11-22 01:40:36 +0900

    Create yet.mm

---
+++ content/post/20241122-daejeon-places/yet.mm
@@ -0,0 +1,58 @@
+---
+title: "장소 추천 -- 대전은 노잼이 아니다!?"
+summary: "그냥 여러분들이 밖을 안다닐뿐..."
+date: 2024-11-22
+
+# Featured image
+# Place an image named `featured.jpg/png` in this page's folder and customize its options here.
+# image:
+#   caption: 'Image credit: [**Unsplash**](https://unsplash.com)'
+
+authors:
+  - admin
+
+tags:
+  - Blog
+---
+
+카이스트에 다니다보면 사람들이 대전이 노잼도시라는 말들을 많이 한다.
+
+하지만 나는 솔직히 공감되지 않는다.
+
+특히 카이스트라는 기숙사에 갇혀 사는 특수한 환경의 사람들은, 그 바깥이 어떤지 모를 것이다.
+
+우리나라 사람은 어떤 소비를 해야만 그것을 재미를 얻는다고 착각하는 경우가 있지만, 
+
+(솔직하게 나는 나 자신 스스로와 대화하는것이 왠만한 사람들과 대화하는것 보다 재밌는 경우도 있어서 별로 공감은 안된다.)
+
+굳이 소비를 통해 재미를 얻고자 한다면 다음과 같은 장소를 추천하고 싶다.
+
+그리고 몇가지 내 지인들이 운영하는 장소들도 리스트에 올려놔서 카이스트 학생들이 참고하면 좋을것도 같다.
+
+# 1. 카페 -- 홀리크로스
+
+그냥 가봐라. 차원이 다른 카페다.
+
+https://naver.me/5JJQVN2Z
+
+# 2. 방탈출 -- 큐브이스케이프 대전둔산점
+
+https://naver.me/xhHnwUx5
+
+# 3. 클럽 -- 어나더
+
+https://naver.me/xxxquhhY
+
+# 4. 찜질방 -- 유성온천불가마사우나
+
+https://naver.me/GKoZTMsX
+
+# 5. 샤브샤브 -- 소제 온천집
+
+https://naver.me/x35iYYnL
+
+
+# 6. 
+
+
+
